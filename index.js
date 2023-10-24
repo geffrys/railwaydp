8e0675283f56ff74f383a5ab2cdd805fac2a6e85
geffrys <geffry.ospina@gmail.com>
2023-10-24 17:55:33 -0500

    testing diferent endpoints

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import router from './router.js';
 
 dotenv.config();
 
@@ -10,6 +11,12 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Works!');
 });
+
+app.get('/test', (req, res) => {
+    res.send('Test Works!');
+})
+
+app.use('/router', router)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
